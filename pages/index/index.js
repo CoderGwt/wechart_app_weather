e26d0228cae04ede3261d3f8477529ddce67dfa5
CoderGwt <1341680843@qq.com>
2019-03-22 00:32:49 +0800

    get current location weather msg

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,6 +7,55 @@ Page({
     
   },
 
+  getWeather: function(){
+
+    var self = this;
+
+    wx.getLocation({
+      success: function(res) {
+        console.log(res);
+
+        var latitude = res.latitude.toFixed(5);
+        var longitude = res.longitude.toFixed(5);
+        
+        wx.request({
+          url: 'https://wx.maoyan.com/hostproxy/locate/v2/rgeo',
+          data: {
+            coord: [latitude,longitude,1].join(",")
+          },
+          header: {
+            // 反爬机制吧应该
+            "x-host": "http://apimobile.vip.sankuai.com"  
+          }, 
+          method: 'get',
+          success: function(info){
+            console.log(info);
+            console.log(info.data.data.city)
+            self.setData({
+              city: info.data.data.city
+            });
+
+            // 请求到城市之后，就获取该城市的天气情况
+            wx.request({
+              url: 'http://apis.juhe.cn/simpleWeather/query',
+              data: {
+                city: self.data.city,
+                key: "3de2c1e1c5080d23987fc22e1dc3b3d5",
+              },
+              method: 'get',
+              success: function(msg){
+                console.log(msg)
+              }
+            })
+
+
+          }
+        })
+
+      },
+    });
+  },
+
   /**
    * 生命周期函数--监听页面加载
    */
